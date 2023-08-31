65b05b28638a4300d9b305b69d1eb72185efb23d
Obee17 <gerrypidgeon@outlook.com>
2023-08-31 12:47:02 +0100

    Updated

--- deliverect.py
+++ deliverect.py
@@ -38,18 +38,18 @@ def process_deliverect_data():
     # Drop Cancelled Duplicates
     df['Status'] = df['Status'].replace('CANCELED', 'CANCEL')
     df = df.drop_duplicates()
-    
+
     # Clean Created Time
     df['AdjustedCreatedTime'] = pd.to_datetime(df['CreatedTimeUTC']).dt.tz_localize('UTC')
     df['AdjustedCreatedTime'] = df['AdjustedCreatedTime'].dt.tz_convert('Europe/Berlin')
     df['OrderDate'] = df['AdjustedCreatedTime'].dt.date
     df['OrderTime'] = df['AdjustedCreatedTime'].dt.time
-    
+
     # Clean Pickup Time
     df['AdjustedPickupTime'] = pd.to_datetime(df['PickupTimeUTC']).dt.tz_localize('UTC')
     df['AdjustedPickupTime'] = df['AdjustedPickupTime'].dt.tz_convert('Europe/Berlin')
     df['PickupTime'] = df['AdjustedPickupTime'].dt.time
-    
+
     # Clean Scheduled Time
     try:
         df['AdjustedScheduledTime'] = pd.to_datetime(df['ScheduledTimeUTC']).dt.tz_localize('UTC')
@@ -57,7 +57,7 @@ def process_deliverect_data():
         df['ScheduledTime'] = df['AdjustedScheduledTime'].dt.time
     except Exception:
         df['ScheduledTime'] = np.nan
-    
+
     # Clean Location
     cleaned_names = pd.read_csv(r'H:\Shared drives\99 - Data\03 - Rx Name List\Full Rx List, with Cleaned Names.csv')
     df['Location'] = df['Location'].str.replace('ö', 'o').str.replace('ü', 'u')
@@ -65,7 +65,7 @@ def process_deliverect_data():
     df = df.rename(columns={'Cleaned Name': 'Cleaned Location'})
     df['Location'] = df['Cleaned Location']
     df = df.drop(columns=['Cleaned Location'])
-    
+
     # Clean Other Columns
     df['Channel'] = df['Channel'].str.replace('TakeAway Com', 'Lieferando')
     df['OrderStatus'] = df['Status'].str.replace('_', ' ').str.title()
@@ -76,55 +76,55 @@ def process_deliverect_data():
     df['GrossAOV'] = df['SubTotal']
     df['NetAOV'] = df['PaymentAmount']
     df['Tips'] = df['Tip']
-    
+
     # Clean Food Panda OrderID
     df['OrderID'] = np.where(df['Channel'] == 'Food Panda', df['OrderID'].str.split(' ', n=1).str[0], df['OrderID'])
-    
+
     # Create New Columns
     df['PrimaryKey'] = df['OrderID'] + ' - ' + df['Location'] + ' - ' + df['OrderDate'].astype(str)
-    
+
     # Drop Columns that aren't needed
     columns_to_drop = ['CreatedTimeUTC', 'PickupTimeUTC', 'ScheduledTimeUTC', 'ReceiptID', 'Note', 'ChannelLink', 'Tax', 'FailureMessage', 'LocationID', 'PosLocationID', 'ChannelLinkID',
                        'OrderTotalAmount', 'CustomerAuthenticatedUserId', 'DeliveryTimeInMinutes', 'PreparationTimeInMinutes', 'AdjustedCreatedTime', 'AdjustedPickupTime', 'AdjustedScheduledTime', 'Type',
                        'Voucher', 'Payment', 'PaymentAmount', 'DiscountTotal', 'SubTotal', 'PaymentAmount', 'Tip', 'Status', 'DeliveryCost', 'CreatedTime']
     df.drop(columns=columns_to_drop, inplace=True)
-    
+
     # Sort Columns
     cols = df.columns.tolist()
     first_cols = ['OrderDate', 'OrderTime', 'PickupTime', 'ScheduledTime', 'Location', 'Brands', 'OrderID', 'Channel', 'OrderStatus', 'DeliveryType', 'PaymentType', 'NetAOV', 'Rebate', 'ServiceCharge', 'DeliveryFee', 'Discounts', 'Tips', 'GrossAOV', 'VAT', 'IsTestOrder', 'ProductPLUs', 'ProductNames', 'PrimaryKey']
     remaining_cols = [col for col in cols if col not in first_cols]
     df = df[first_cols + remaining_cols]
-    
+
     # Sort Rows
     df = df.sort_values(by=['OrderDate', 'OrderTime'])
-    
+
     # Align Columns Names
     df = df.rename(columns={'Discounts': 'PromotionsOnItems'})
-    
+
     # Standardise Brands
     df['Brands'] = np.where (df['Brands'].isnull(), 'Birdie Birdie', df['Brands'])
     df['Loc with Brand'] = df['Location'] + ' - ' + df['Brands'].str.split(n=1).str[0]
-    
+
     # Standardise Columns
     column_order = ['PrimaryKey', 'Location', 'Loc with Brand', 'Brands', 'OrderID', 'OrderDate', 'OrderTime', 'Channel', 'OrderStatus', 'DeliveryType', 'GrossAOV', 'PromotionsOnItems', 'DeliveryFee', 'Tips', 'ProductPLUs', 'ProductNames', 'IsTestOrder', 'PaymentType', 'PickupTime']
     df = df[column_order]
-    
+
     # Reset Index
     df = df.reset_index(drop=True)
-    
+
     # Remove Duplicate Primary Keys
     cleaned_df = df['PrimaryKey']
     cleaned_df = cleaned_df.drop_duplicates()
-    
+
     # Create new DataFrame
     df = df[df.index.isin(cleaned_df.index)]
-    
+
     # Filter Lieferando Test Order
     df = df.loc[df['OrderID'] != '#63H1WT']
-    
+
     # Change to Data Directory
     os.chdir(r'H:\Shared drives\99 - Data\00 - Cleaned Data')
-    
+
     # Export File
     df.to_csv('Deliverect Data.csv', index=False)
 
